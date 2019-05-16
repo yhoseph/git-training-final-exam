8b0ee857fab4c73630d3800664ef3fbf5ed787ed
Ariel <ariel.lenisd@gmail.com>
2019-05-16 01:35:28 -0400

    First look to the fractal.

--- src/snowflake-fractal.js
+++ src/snowflake-fractal.js
@@ -2,14 +2,28 @@ function toRadian(angle) {
     return angle * Math.PI / 180;
 }
 
-function drawSnowFlakeLine(context, x0, y0, angle0, length) {
-    let x1 = x0 + length * Math.cos(angle0),
-        y1 = y0 - length * Math.sin(angle0);
+function drawSnowFlakeLine(context, x0, y0, angle0, length, deep) {
+    if (deep == 0) {
+        let x1 = x0 + length * Math.cos(angle0),
+            y1 = y0 - length * Math.sin(angle0);
 
-    context.beginPath();
-    context.moveTo(x0, y0);
-    context.lineTo(x1, y1);
-    context.stroke();
+        context.beginPath();
+        context.moveTo(x0, y0);
+        context.lineTo(x1, y1);
+        context.stroke();
+    } else {
+        let angles = Array(0, -60, 120, -60);
+        let xi = x0,
+            yi = y0,
+            anglei = angle0,
+            newLength = length / 3;
+        for(let i=0;i<angles.length;i++) {
+            anglei+=toRadian(angles[i]);
+            drawSnowFlakeLine(context, xi, yi, anglei, newLength, deep - 1);
+            xi = xi + newLength * Math.cos(anglei);
+            yi = yi - newLength * Math.sin(anglei);
+        }
+    }
 }
 
 function drawSnowFlake(context, width, height, ratius) {
@@ -24,6 +38,6 @@ function drawSnowFlake(context, width, height, ratius) {
         let angle0 = radAngle + 5 / 6 * Math.PI;
         let length = Math.sqrt(3) * ratius;
 
-        drawSnowFlakeLine(context, x0, y0, angle0, length);
+        drawSnowFlakeLine(context, x0, y0, angle0, length, 4);
     }
 }
